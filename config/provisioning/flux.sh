4e457b3e38420b30d722fac3ee120c5fff0a792f
Grae <graen.ai.ar@gmail.com>
2025-06-11 15:28:17 -0600

    Update flux.sh

--- config/provisioning/flux.sh
+++ config/provisioning/flux.sh
@@ -3,5 +3,4 @@
 curl -H "$AUTH_PROV" -Lo- "$PROVISIONING_SCRIPT" > /secure_provisioning.sh
 chmod +x /secure_provisioning.sh && \
 (set -o pipefail; /secure_provisioning.sh 2>&1 | tee -a /var/log/portal/secure_provisioning.log) && \
-touch /.provisioning_complete && \
 echo "Secure Provisioning complete!" | tee -a /var/log/portal/secure_provisioning.log
