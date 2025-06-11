2e8a08fe413093160982c121f0c29ad9e0ecec52
Grae <graen.ai.ar@gmail.com>
2025-06-11 16:17:05 -0600

    Update flux.sh

--- config/provisioning/flux.sh
+++ config/provisioning/flux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -H "Authorization: Bearer $AUTH_PROV" -Lo- "$AUTH_PROVISION" > /secure_provisioning.sh
+curl -H "Authorization: Bearer $AUTH_PAT" -Lo- "$AUTH_PROVISION" > /secure_provisioning.sh
 chmod +x /secure_provisioning.sh && \
 (set -o pipefail; /secure_provisioning.sh 2>&1 | tee -a /var/log/portal/secure_provisioning.log) && \
 echo "Secure Provisioning complete!" | tee -a /var/log/portal/secure_provisioning.log
